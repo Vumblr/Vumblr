0763dce025421cff3146132907801605d7b22e71
krze <krze@tumblr.com>
2015-09-30 17:42:57 -0400

    Edit video view has a subview where video plays, button to add sticker (nonfunctional)

---
+++ AddStickerToVideo.swift
@@ -0,0 +1,9 @@
+//
+//  AddStickerToVideo.swift
+//  VideoTest
+//
+//  Created by Ken Krzeminski on 9/30/15.
+//  Copyright © 2015 Ken Krzeminski. All rights reserved.
+//
+
+import Foundation
---
+++ VideoTest/PlayerView.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerView.swift
+//  VideoTest
+//
+//  Created by Ken Krzeminski on 9/30/15.
+//  Copyright © 2015 Ken Krzeminski. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    
+    @IBOutlet var videoPlayerView: UIView!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
