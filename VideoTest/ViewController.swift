4749581ad0209a22d99536f392aaddd3ffb5dcc4
Lea <lea@tumblr.com>
2015-10-02 12:33:31 -0400

    add UI design

--- VideoTest/ViewController.swift
+++ VideoTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
