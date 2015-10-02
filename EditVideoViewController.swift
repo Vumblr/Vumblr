e1712245673a1b4c5758be6a1f8837b9cfe7dbb1
Ken Krzeminski <krze@tumblr.com>
2015-10-01 22:42:20 -0400

    I still don't know what I'm doing

--- EditVideoViewController.swift
+++ EditVideoViewController.swift
@@ -31,105 +31,138 @@ class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate
     }
     
     func addSticker() {
-        let composition = AVMutableComposition()
-        let vidAsset = AVURLAsset(URL: selectedFileUrl!, options: nil)
+        var mergeComposition : AVMutableComposition = AVMutableComposition()
+        var trackVideo : AVMutableCompositionTrack = mergeComposition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        //var trackAudio : AVMutableCompositionTrack = mergeComposition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: CMPersistentTrackID())
         
-        // get video track
-        let vtrack =  vidAsset.tracksWithMediaType(AVMediaTypeVideo)
-        let videoTrack:AVAssetTrack = vtrack[0]
-//        let vid_duration = videoTrack.timeRange.duration
-        let vid_timerange = CMTimeRangeMake(kCMTimeZero, vidAsset.duration)
+        // 2. Add a bank for theme insertion later
         
-        let compositionvideoTrack:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        //trackVideo.insertTimeRange(range, ofTrack: VideoHelper.Static.blankTrack, atTime: kCMTimeZero, error: nil)
         
-        do {
-            try compositionvideoTrack.insertTimeRange(vid_timerange, ofTrack: videoTrack, atTime: kCMTimeZero)
-        } catch  {
-            print("Error inserting time range into \(videoTrack)")
+        // 3. Source tracks
+        
+        let sourceAsset = AVURLAsset(URL: selectedFileUrl!, options: nil)
+        let sourceDuration = CMTimeRangeMake(kCMTimeZero, sourceAsset.duration)
+        let vtrack: AVAssetTrack? = sourceAsset.tracksWithMediaType(AVMediaTypeVideo)[0] as AVAssetTrack
+        let atrack: AVAssetTrack? = sourceAsset.tracksWithMediaType(AVMediaTypeAudio)[0] as AVAssetTrack
+        
+        if (vtrack == nil) {
+            return
         }
         
-        compositionvideoTrack.preferredTransform = videoTrack.preferredTransform
+        let renderWidth = vtrack?.naturalSize.width
+        let renderHeight = vtrack?.naturalSize.height
+        let insertTime = kCMTimeZero
+        let endTime = sourceAsset.duration
+        let range = sourceDuration
         
-        // Set the size of the video
-        let size = videoTrack.naturalSize
-        
-        // Watermark Effect
-        
-//        
-//        let imglogo = UIImage(named: "image.png")
-//        let imglayer = CALayer()
-//        imglayer.contents = imglogo?.CGImage
-//        imglayer.frame = CGRectMake(5, 5, 100, 100)
-//        imglayer.opacity = 0.6
-//        
-        // create text Layer
-        let titleLayer = CATextLayer()
-        titleLayer.backgroundColor = UIColor.whiteColor().CGColor
-        titleLayer.string = "Dummy text"
-        titleLayer.font = UIFont(name: "Helvetica", size: 28)
-        titleLayer.shadowOpacity = 0.5
-        titleLayer.alignmentMode = kCAAlignmentCenter
-        titleLayer.frame = CGRectMake(0, 50, size.width, size.height / 6)
-        
-        let videolayer = CALayer()
-        videolayer.frame = CGRectMake(0, 0, size.width, size.height)
-        
-        let parentlayer = CALayer()
-        parentlayer.frame = CGRectMake(0, 0, size.width, size.height)
-        parentlayer.addSublayer(videolayer)
-//        parentlayer.addSublayer(imglayer)
-        parentlayer.addSublayer(titleLayer)
-        
-        let layercomposition = AVMutableVideoComposition()
-        layercomposition.frameDuration = CMTimeMake(1, 30)
-        layercomposition.renderSize = size
-        layercomposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videolayer, inLayer: parentlayer)
-        
-        // instruction for watermark
-        let instruction = AVMutableVideoCompositionInstruction()
-        instruction.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration)
-        let videotrack = composition.tracksWithMediaType(AVMediaTypeVideo)[0] as AVAssetTrack
-        let layerinstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videotrack)
-        instruction.layerInstructions = NSArray(object: layerinstruction) as! [AVVideoCompositionLayerInstruction]
-        layercomposition.instructions = NSArray(object: instruction) as! [AVVideoCompositionInstructionProtocol]
-        
-        //  create new file to receive data
-        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-        let docsDir: AnyObject = dirPaths[0]
-        let movieFilePath = docsDir.stringByAppendingPathComponent("result.mov")
-        let movieDestinationUrl = NSURL(fileURLWithPath: movieFilePath)
-        
-        // use AVAssetExportSession to export video
-        let assetExport = AVAssetExportSession(asset: composition, presetName:AVAssetExportPresetHighestQuality)
-        assetExport!.outputFileType = AVFileTypeQuickTimeMovie
-        assetExport!.outputURL = movieDestinationUrl
-        assetExport!.exportAsynchronouslyWithCompletionHandler({
-            switch assetExport!.status{
-            case  AVAssetExportSessionStatus.Failed:
-                print("failed \(assetExport!.error)")
-            case AVAssetExportSessionStatus.Cancelled:
-                print("cancelled \(assetExport!.error)")
-            default:
-               print("Movie complete")
-                
-                
-                // play video
-                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-                    let player = AVPlayer(URL: movieDestinationUrl)
-                    let playerController = AVPlayerViewController()
-                    
-                    playerController.player = player
-                    //        self.addChildViewController(playerController)
-                    self.playerView.addSubview(playerController.view)
-                    playerController.view.frame = self.playerView.bounds
-                    
-                    
-                    player.play()
+        // append tracks
+        do {
+            try trackVideo.insertTimeRange(sourceDuration, ofTrack: vtrack!, atTime: insertTime)
+        }catch {
+            print("error inserting time range")
+        }
 
-//                    playerView.playVideo()
-                })
+        // 4. Add subtitles (we call it theme)
+        
+        var themeVideoComposition : AVMutableVideoComposition = AVMutableVideoComposition(propertiesOfAsset: sourceAsset)
+        
+        // 4.1 - Create AVMutableVideoCompositionInstruction
+        
+        let mainInstruction: AVMutableVideoCompositionInstruction = AVMutableVideoCompositionInstruction()
+        mainInstruction.timeRange = range
+        
+        // 4.2 - Create an AVMutableVideoCompositionLayerInstruction for the video track and fix the orientation.
+        
+        let videolayerInstruction : AVMutableVideoCompositionLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: trackVideo)
+        videolayerInstruction.setTransform(trackVideo.preferredTransform, atTime: insertTime)
+        videolayerInstruction.setOpacity(0.0, atTime: endTime)
+        
+        // 4.3 - Add instructions
+        
+        mainInstruction.layerInstructions = NSArray(array: [videolayerInstruction]) as! [AVVideoCompositionLayerInstruction]
+        
+        themeVideoComposition.renderScale = 1.0
+        themeVideoComposition.renderSize = CGSizeMake(renderWidth!, renderHeight!)
+        themeVideoComposition.frameDuration = CMTimeMake(1, 30)
+        themeVideoComposition.instructions = NSArray(array: [mainInstruction]) as! [AVVideoCompositionInstructionProtocol]
+        
+        // add the theme
+        
+        // setup variables
+        
+        // add text
+        
+        let title = String("Testing this subtitle")
+        
+        var titleLayer = CATextLayer()
+        titleLayer.string = title
+        titleLayer.frame =  CGRect(x: 0, y: 0, width: renderWidth!, height: renderHeight!)
+        let fontName: CFStringRef = "Helvetica-Bold"
+        let fontSize = CGFloat(36)
+        titleLayer.font = CTFontCreateWithName(fontName, fontSize, nil)
+        titleLayer.alignmentMode = kCAAlignmentCenter
+        titleLayer.foregroundColor = UIColor.whiteColor().CGColor
+        
+        var backgroundLayer = CALayer()
+        backgroundLayer.frame = CGRect(x: 0, y: 0, width: renderWidth!, height: renderHeight!)
+        backgroundLayer.masksToBounds = true
+        backgroundLayer.addSublayer(titleLayer)
+        
+        // 2. set parent layer and video layer
+        
+        var parentLayer = CALayer()
+        var videoLayer = CALayer()
+        parentLayer.frame =  CGRect(x: 0, y: 0, width: renderWidth!, height: renderHeight!)
+        videoLayer.frame =  CGRect(x: 0, y: 0, width: renderWidth!, height: renderHeight!)
+        
+        parentLayer.addSublayer(backgroundLayer)
+        parentLayer.addSublayer(videoLayer)
+        
+        // 3. make animation
+        
+        themeVideoComposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, inLayer: parentLayer)
+        
+        // Remove the file if it already exists (merger does not overwrite)
+        let filename = "composition.mp4"
+        let outputPath = NSTemporaryDirectory().stringByAppendingString(filename)
+        let outputUrl = NSURL(fileURLWithPath: outputPath)
+        let fileManager = NSFileManager.defaultManager()
+        do {
+         try fileManager.removeItemAtURL(outputUrl)
+        } catch {
+            print("Unable to remove item at \(outputUrl)")
+        }
+        
+        // export to output url
+        
+        let exporter = AVAssetExportSession(asset: mergeComposition, presetName: AVAssetExportPresetHighestQuality)
+        exporter!.outputURL = outputUrl
+        print(exporter!.outputURL)
+        exporter!.videoComposition = themeVideoComposition
+        exporter!.outputFileType = AVFileTypeQuickTimeMovie
+        exporter!.shouldOptimizeForNetworkUse = true
+        exporter!.exportAsynchronouslyWithCompletionHandler({
+            if (exporter!.error != nil) {
+                print("Error")
+                print(exporter!.error)
+                print("Description")
+                print(exporter!.description)
             }
+            let player = AVPlayer(URL: exporter!.outputURL!)
+            let playerController = AVPlayerViewController()
+            
+            playerController.player = player
+            //        self.addChildViewController(playerController)
+            self.playerView.addSubview(playerController.view)
+            playerController.view.frame = self.playerView.bounds
+            
+            
+            player.play()
+//            completionHandler(exporter.status.rawValue)
         })
+        
+
     }
     
     func presentPicker() {
@@ -144,8 +177,7 @@ class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         print(info)
         self.dismissViewControllerAnimated(true, completion: nil)
-        let infoURL = info["UIImagePickerControllerMediaURL"]
-        let fileURL = NSURL(fileURLWithPath: "\(infoURL)")
+        let fileURL = info["UIImagePickerControllerMediaURL"] as! NSURL
         selectedFileUrl = fileURL
         let player = AVPlayer(URL: fileURL)
         let playerController = AVPlayerViewController()
