e30f96df36bfdc08d1a0d28e8279f38baaf4ee79
Ken Krzeminski <krze@tumblr.com>
2015-10-01 21:56:58 -0400

    I have no idea what i'm doing

--- EditVideoViewController.swift
+++ EditVideoViewController.swift
@@ -15,8 +15,7 @@ import VideoToolbox
 
 class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var mixComposition = AVMutableComposition()
-    
+    var selectedFileUrl: NSURL?
     
     @IBOutlet weak var playerView: PlayerView!
 
@@ -32,9 +31,105 @@ class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate
     }
     
     func addSticker() {
-        let stickerTrack = mixComposition.addMutableTrackWithMediaType(AVMediaTypeText, preferredTrackID: Int32(kCMPersistentTrackID_Invalid))
+        let composition = AVMutableComposition()
+        let vidAsset = AVURLAsset(URL: selectedFileUrl!, options: nil)
         
-//        stickerTrack.insert
+        // get video track
+        let vtrack =  vidAsset.tracksWithMediaType(AVMediaTypeVideo)
+        let videoTrack:AVAssetTrack = vtrack[0]
+//        let vid_duration = videoTrack.timeRange.duration
+        let vid_timerange = CMTimeRangeMake(kCMTimeZero, vidAsset.duration)
+        
+        let compositionvideoTrack:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: CMPersistentTrackID())
+        
+        do {
+            try compositionvideoTrack.insertTimeRange(vid_timerange, ofTrack: videoTrack, atTime: kCMTimeZero)
+        } catch  {
+            print("Error inserting time range into \(videoTrack)")
+        }
+        
+        compositionvideoTrack.preferredTransform = videoTrack.preferredTransform
+        
+        // Set the size of the video
+        let size = videoTrack.naturalSize
+        
+        // Watermark Effect
+        
+//        
+//        let imglogo = UIImage(named: "image.png")
+//        let imglayer = CALayer()
+//        imglayer.contents = imglogo?.CGImage
+//        imglayer.frame = CGRectMake(5, 5, 100, 100)
+//        imglayer.opacity = 0.6
+//        
+        // create text Layer
+        let titleLayer = CATextLayer()
+        titleLayer.backgroundColor = UIColor.whiteColor().CGColor
+        titleLayer.string = "Dummy text"
+        titleLayer.font = UIFont(name: "Helvetica", size: 28)
+        titleLayer.shadowOpacity = 0.5
+        titleLayer.alignmentMode = kCAAlignmentCenter
+        titleLayer.frame = CGRectMake(0, 50, size.width, size.height / 6)
+        
+        let videolayer = CALayer()
+        videolayer.frame = CGRectMake(0, 0, size.width, size.height)
+        
+        let parentlayer = CALayer()
+        parentlayer.frame = CGRectMake(0, 0, size.width, size.height)
+        parentlayer.addSublayer(videolayer)
+//        parentlayer.addSublayer(imglayer)
+        parentlayer.addSublayer(titleLayer)
+        
+        let layercomposition = AVMutableVideoComposition()
+        layercomposition.frameDuration = CMTimeMake(1, 30)
+        layercomposition.renderSize = size
+        layercomposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videolayer, inLayer: parentlayer)
+        
+        // instruction for watermark
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRangeMake(kCMTimeZero, composition.duration)
+        let videotrack = composition.tracksWithMediaType(AVMediaTypeVideo)[0] as AVAssetTrack
+        let layerinstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videotrack)
+        instruction.layerInstructions = NSArray(object: layerinstruction) as! [AVVideoCompositionLayerInstruction]
+        layercomposition.instructions = NSArray(object: instruction) as! [AVVideoCompositionInstructionProtocol]
+        
+        //  create new file to receive data
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docsDir: AnyObject = dirPaths[0]
+        let movieFilePath = docsDir.stringByAppendingPathComponent("result.mov")
+        let movieDestinationUrl = NSURL(fileURLWithPath: movieFilePath)
+        
+        // use AVAssetExportSession to export video
+        let assetExport = AVAssetExportSession(asset: composition, presetName:AVAssetExportPresetHighestQuality)
+        assetExport!.outputFileType = AVFileTypeQuickTimeMovie
+        assetExport!.outputURL = movieDestinationUrl
+        assetExport!.exportAsynchronouslyWithCompletionHandler({
+            switch assetExport!.status{
+            case  AVAssetExportSessionStatus.Failed:
+                print("failed \(assetExport!.error)")
+            case AVAssetExportSessionStatus.Cancelled:
+                print("cancelled \(assetExport!.error)")
+            default:
+               print("Movie complete")
+                
+                
+                // play video
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                    let player = AVPlayer(URL: movieDestinationUrl)
+                    let playerController = AVPlayerViewController()
+                    
+                    playerController.player = player
+                    //        self.addChildViewController(playerController)
+                    self.playerView.addSubview(playerController.view)
+                    playerController.view.frame = self.playerView.bounds
+                    
+                    
+                    player.play()
+
+//                    playerView.playVideo()
+                })
+            }
+        })
     }
     
     func presentPicker() {
@@ -49,7 +144,10 @@ class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         print(info)
         self.dismissViewControllerAnimated(true, completion: nil)
-        let player = AVPlayer(URL: info["UIImagePickerControllerMediaURL"] as! NSURL)
+        let infoURL = info["UIImagePickerControllerMediaURL"]
+        let fileURL = NSURL(fileURLWithPath: "\(infoURL)")
+        selectedFileUrl = fileURL
+        let player = AVPlayer(URL: fileURL)
         let playerController = AVPlayerViewController()
         
         playerController.player = player
@@ -64,6 +162,7 @@ class EditVideoViewController: UIViewController, UIImagePickerControllerDelegate
     
     @IBAction func onClickAddSticker(sender: AnyObject) {
         print("Added sticker")
+        addSticker()
     }
     
     /*
