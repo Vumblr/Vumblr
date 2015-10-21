3405898ca6357cb020f5fb50fadc98e589696d98
Andy Dong <ldong@tumblr.com>
2015-10-20 20:22:58 -0400

    adding support for scale and rotation in Sticker class

--- VideoTest/Sticker.swift
+++ VideoTest/Sticker.swift
@@ -17,4 +17,17 @@ class Sticker: NSObject {
     var y: CGFloat?
     var height: CGFloat?
     var width: CGFloat?
+    var scale: CGFloat?
+    var rotation: CGFloat? // radian
+    
+    func updateSticker(imageView: UIImageView, paddingTop: CGFloat) {
+        x = imageView.center.x - imageView.bounds.width / 2
+        y = imageView.center.y - imageView.bounds.height / 2 - paddingTop
+        width = imageView.bounds.width
+        height = imageView.bounds.height
+    }
+    
+    func toString() -> String {
+        return "timestamp: \(timestamp), x: \(x), y: \(y), height: \(height), width: \(width), scale: \(scale), rotation:\(rotation)"
+    }
 }
